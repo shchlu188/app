956d6beac2d0ffa42ada64610f5eee98a97ddc31
shchlu188 <shchlu188@163.com>
2020-05-21 09:00:50 +0800

    git------first commit

---
+++ scl-parent/src/main/java/com/scl/GitDemo.java
@@ -0,0 +1,16 @@
+package com.scl;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @version v1.0.0
+ * @package: com.scl
+ * @author: chenglu
+ * @create: 2020/5/21
+ * @desc:
+ */
+public class GitDemo {
+    public static void main(String[] args){
+        System.out.println("git hello..................");
+    }
+}
